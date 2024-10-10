2ba6f99e57b21eed9625d1189c4d800666f23d8e
Gusuyoung <suyoung606123@gmail.com>
2024-10-10 11:36:05 +0900

    thirth

---
+++ src/main/java/com/green/day03/ch05/MissionWhile1.java
@@ -0,0 +1,19 @@
+package com.green.day03.ch05;
+
+public class MissionWhile1 {
+    public static void main(String[] args) {
+        /*
+        while문을 활용하여 1~99까지의 합을 구하는 프로그램을 작성하시오.
+        1. 1~99의 값을 만들 수 있어야 한다.
+        2. 만든 값을 중쳡해서 저장할 수 있어야 한다.
+         */
+
+        int n = 1;
+        int sum = 0;
+        while(n < 100) {
+            //sum = sum + (n++);
+            sum += n++;
+        }
+        System.out.println("result? = " + sum);
+    }
+}
