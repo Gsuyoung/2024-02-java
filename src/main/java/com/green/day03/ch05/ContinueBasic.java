0106cee375f45b64d8548c4ed4254e7f9f090f7e
Gusuyoung <suyoung606123@gmail.com>
2024-10-10 15:03:24 +0900

    thirth2

---
+++ src/main/java/com/green/day03/ch05/ContinueBasic.java
@@ -0,0 +1,17 @@
+package com.green.day03.ch05;
+
+public class ContinueBasic {
+    public static void main(String[] args) {
+        int n = 0;
+        int count = 0;
+
+        while((n++) < 100) { //<--(n++먼저 실행되서) 제일 처음 실행되는 n값은 1
+            if(((n % 5) != 0) || ((n % 7) != 0)) {
+                continue;
+            }
+            count++;
+            System.out.println("n: " + n);
+        }
+        System.out.println("count: " + count);
+    }
+}
---
+++ src/main/java/com/green/day03/ch05/MissionContinue.java
@@ -0,0 +1,17 @@
+package com.green.day03.ch05;
+
+public class MissionContinue {
+    public static void main(String[] args) {
+        /* ContinueBasic의 존재하는 while문을 for문으로 변경해보자. */
+
+        int count = 0;
+        for(int n = 1; n < 100; n++) {
+            if(((n % 5) != 0) || ((n % 7) != 0)) {
+                continue;
+            }
+            count++;
+            System.out.println("n: " + n);
+        }
+        System.out.println("count: " + count);
+    }
+}
