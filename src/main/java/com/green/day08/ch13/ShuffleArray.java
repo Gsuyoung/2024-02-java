e5f1e5188004aa36ad9b1dd720818f7f302fcddf
Gusuyoung <suyoung606123@gmail.com>
2024-10-17 17:26:12 +0900

    d4

--- src/main/java/com/green/day08/ch13/ShuffleArray.java
+++ src/main/java/com/green/day08/ch13/ShuffleArray.java
@@ -6,11 +6,23 @@ public class ShuffleArray {
     public static void main(String[] args) {
         int[] arr = new int[20];
 
-        //1~20순차적으로 값을 넗어주세요.
-        for (int i=0; i<arr.length; i++) {
-            arr[i]=i+1;
+        //1 ~ 20순차적으로 값을 넣어주세요.
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        int temp;
+
+        //0 ~ 19 랜덤값
+        for (int i = 0; i < arr.length; i++) {
+            int ridx = (int)(Math.random() * arr.length);
+
+            temp = arr[i];
+            arr[i] = arr[ridx];
+            arr[ridx] = temp;
 
         }
-        System.out.println(Arrays.toString(arr));
+
+            System.out.println(Arrays.toString(arr));
     }
 }
